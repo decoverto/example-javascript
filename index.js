0a6230ec563bbbdd3dde9680c37ce4a023beeb70
Matthias Kunnen <matthias.kunnen@gmail.com>
2021-04-04 15:19:57 +0200

    Comment all example imports

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ console.log(`Examples can be found in 'src/'. Import an example in index.js or o
 your own code. Keep the browser window open to view errors.`)
 
 // Uncomment one of these imports or write your own code
-require('./src/basic-class');
-require('./src/inheritance-discriminator');
-require('./src/inheritance-predicate');
-require('./src/mapped-types');
-require('./src/nested-class');
-require('./src/overriding-converters')
+// require('./src/basic-class');
+// require('./src/inheritance-discriminator');
+// require('./src/inheritance-predicate');
+// require('./src/mapped-types');
+// require('./src/nested-class');
+// require('./src/overriding-converters')
